c86410802a8353520e990a72af5ecbc88887c964
lynx5001 <info.lcr@protonmail.com>
2025-09-02 21:50:31 +0200

    adding cli menu interaction

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use clearscreen;
 
 // create task struct
 struct Task {
@@ -7,25 +8,10 @@ struct Task {
     // other properties like note, list type, deadline, priority...
 }
 
-// add task to list
-fn add_new_task(mut p_todo_list: Vec<Task>) -> Vec<Task> {
+fn add_new_task(mut p_todo_list: Vec<Task>, p_new_task: Task) -> Vec<Task> {
 
-    // create new task
-    let mut new_task = Task {
-        name: String::new(),
-        // default not checked
-        is_checked: false,
-    };
-
-    // ask user for name of task and read input
-    println!("Enter name of new task:");
-    io::stdin().read_line(&mut new_task.name).expect("failed to read task name");    
-
-    println!("Added {} to the list!", new_task.name);
-    //add task to todo list
-    p_todo_list.push(new_task);
-    
-
+    //add task to todo list and return todo list
+    p_todo_list.push(p_new_task);
     p_todo_list
 }
 
@@ -40,20 +26,60 @@ fn add_new_task(mut p_todo_list: Vec<Task>) -> Vec<Task> {
 // }
 
 // display current todo list
-fn display_todo_list(p_todo_list: Vec<Task>) {
+fn display_todo_list(p_todo_list: &Vec<Task>) {
 
-    println!{"Current Todo-List: "}
+    println!{"Todo-List: "}
     println!{"--------------------"}
     for task in p_todo_list {
-        println!("{}", task.name);
+        if !task.is_checked {
+            println!("{}", task.name);
+        }
     }
 }
 
 fn main() {
-    let mut todo_list: Vec<Task> = Vec::new();
-    todo_list = add_new_task(todo_list);
-    todo_list = add_new_task(todo_list);
-    todo_list = add_new_task(todo_list);
 
-    display_todo_list(todo_list);
+    // create list
+    let mut todo_list: Vec<Task> = Vec::new();
+    clearscreen::clear().expect("Failed to clear screen.");
+
+    // welcome message
+    println!("Welcome to your todo-list powered by rust!\n");
+    
+    loop{
+        let mut option = String::new();
+
+        // menu for todo-list
+        println!("\nSelect option for you todo-list: ");
+        println!("s - show current todo-list");
+        println!("a - add task to todo-list");
+        println!("e - edit task on todo-list");
+        println!("c - check task on todo-list");
+        println!("h - see checked tasks");
+        println!("q - quit application");
+
+
+        // take user input
+        io::stdin().read_line(&mut option).expect("Failed to read option!");    
+        // switch case to activate function 
+        match option.trim() {
+            "s" => display_todo_list(&todo_list),
+            "a" => {
+                // create new task
+                let mut new_task = Task {
+                    name: String::new(),
+                    is_checked: false,
+                };
+
+                // ask user for task name
+                println!("\nEnter task name: ");
+                io::stdin().read_line(&mut new_task.name).expect("Failed to read task name!");    
+
+                todo_list = add_new_task(todo_list, new_task);
+            },
+            "q" => {break;},
+            _ => println!("\nWork in progress, option not available!"), 
+        }
+    }
+    println!("\nThank you for using rust todo list!");
 }
